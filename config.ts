f7cddeb597986b7e20a9732dcb29155942212fe4
hiro <pracube0818@gmail.com>
2025-12-13 22:48:13 +0900

    change : envのurlの末尾スラッシュを削除、それに伴うconfig.tsの修正

--- config.ts
+++ config.ts
@@ -1,7 +1,5 @@
-export const API_URL =
-    process.env.NEXT_PUBLIC_DJANGO_BASE_URL ?? "http://localhost:8000/";
+export const API_URL = process.env.NEXT_PUBLIC_DJANGO_BASE_URL;
 
-export const FRONT_URL =
-    process.env.NEXT_PUBLIC_FRONT_BASE_URL ?? "http://localhost:3000/";
+export const FRONT_URL = process.env.NEXT_PUBLIC_FRONT_BASE_URL;
 
-export const LOGIN_URL = `${API_URL}accounts/login/`;
+export const LOGIN_URL = `${API_URL}/accounts/login/`;
