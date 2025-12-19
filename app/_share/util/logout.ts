926833e1f6c39efb1b1690803bcbed6ea9e34f94
hiro <pracube0818@gmail.com>
2025-12-19 12:29:02 +0900

    update : 軽微な修正

--- app/_share/util/logout.ts
+++ app/_share/util/logout.ts
@@ -4,7 +4,7 @@ import getCookie from "./getCookie";
 export default async function logout() {
     const csrfToken = getCookie("csrftoken");
 
-    const res = await fetch(`${DJANGO_URL}/accounts/logout/`, {
+    const res = await fetch(`${DJANGO_URL}/accounts/api/logout`, {
         method: "POST",
         credentials: "include",
         headers: csrfToken ? { "X-CSRFToken": csrfToken } : {},
