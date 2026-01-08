efe8350a6a55e1943324a16093ced7c295b6e277
hiro <pracube0818@gmail.com>
2026-01-08 10:02:56 +0900

    a

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        dangerouslyAllowLocalIP: true,
+        //dangerouslyAllowLocalIP: true,
         remotePatterns: [
             {
                 protocol: "https",
