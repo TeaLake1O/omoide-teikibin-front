662295b0711f3adaed1e44c0e78928fd728fdcdd
hiro <pracube0818@gmail.com>
2026-02-14 16:31:50 +0900

    a

--- app/(routes)/camera/page.tsx
+++ app/(routes)/camera/page.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import CameraIcon from "@/app/_share/components/UI/Icon/CameraIcon";
+import usePickImage from "@/app/_share/hooks/util/usePickImage";
+import Image from "next/image";
 
 export default function Camera() {
+    const { url, inputProps, file, imageReset, fileOpen } = usePickImage();
     return (
         <div className="h-full w-full flex justify-center items-center">
-            <button className="h-[25%] w-[25%]">
+            <button className="h-[25%] w-[25%]" onClick={fileOpen}>
                 <CameraIcon />
             </button>
-            <input type="file" accept="image/*" capture />
+            {url && <Image src={url} alt="a" />}
+            <input {...inputProps} accept="image/*" capture />
         </div>
     );
 }
